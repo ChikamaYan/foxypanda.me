8d613823c8874e2e664970cf2cea80ea4358e2bf
TimboKZ <tima_kuzh@inbox.ru>
2016-08-21 19:56:40 +0100

    Add Masonry

---
+++ content/themes/foxy-panda/assets/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * A JavaScript file.
+ *
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ * @version 0.0.1
+ */
+
+$(window).load(function () {
+
+    var $body = $('body');
+
+    if ($body.hasClass('home-template')) {
+
+        var $container = $('#masonry');
+        $container.masonry({
+            itemSelector: '.item',
+            gutter: '.grid-sizer'
+        });
+
+    }
+
+});
