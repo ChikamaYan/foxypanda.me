e79287e7029636471fd132372ba42e6d5fdb509a
TimboKZ <tima_kuzh@inbox.ru>
2016-08-21 19:33:04 +0100

    Install Mueller Grid System

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "content/themes/foxy-panda/assets/css"
+sass_dir = "content/themes/foxy-panda/assets/sass"
+images_dir = "content/themes/foxy-panda/assets/img"
+javascripts_dir = "content/themes/foxy-panda/assets/js"
+
+output_style = :compressed
+line_comments = false
